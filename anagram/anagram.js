76a691fc7fb8ba9fb97f34bb05b051461d1f7cc6
Nathan Delgado <nathandelgado.dv@gmail.com>
2022-02-27 16:39:43 -0500

    File for create a solution

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -1,19 +1,7 @@
 function isAnagram(str1, str2) {
-    if(typeof(str1) !== 'string' && typeof(str2) !== 'string') return false;
+// write your code below this line
 
-    const firstWord = str2.replace(/\s/g, '')
-        .toLowerCase()        
-        .split('')
-        .sort()
-        .join('');
-    const secondWord = str1.replace(/\s/g, '')
-        .toLowerCase()
-        .split('')
-        .sort()
-        .join('');  
-
-    if(firstWord !== secondWord) return false;
-    return true;
+// write your code above this line
 }
 
 module.exports = {
