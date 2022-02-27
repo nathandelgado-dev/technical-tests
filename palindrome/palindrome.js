b095e2a770480564a68422ba8ad594a56b952908
Nathan Delgado <nathandelgado.dv@gmail.com>
2022-02-27 16:55:42 -0500

    File for create a solution

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,16 +1,7 @@
 function isPalindrome(str) {
-    if(typeof(str) !== 'string') return false;
-    const strRight = str.toLowerCase()
-        .replace(/\s/g, '');
-    
-    const strLeft = str.toLocaleLowerCase()
-        .replace(/\s/g, '')
-        .split('')
-        .reverse()
-        .join('')
-    
-    if(strRight === strLeft) return true;
-    return false;
+// write your code below this line
+
+// write your code above this line
 }
 
 module.exports = {
